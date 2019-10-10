7c1743bc75aa25dd8f2c36564dd44ef0a00e2bdc
Shakil Ahmmed <shakilfci461@gmail.com>
2019-10-10 22:49:41 +0600

    Ajax'

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Posts,Category
 
-
+# class FilteredPostSerializer(serializers.ListSerializer):
+#     def to_representation(self,data):
+#        data = data.filter(status=True)
+#        return super(FilteredPostSerializer,  self).to_representation(data)
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -9,11 +12,12 @@ class PostSerializer(serializers.ModelSerializer):
     category_description = serializers.ReadOnlyField(source='category.description')
     class Meta:
         model = Posts
+        #list_serializer_class = FilteredPostSerializer
         fields = "__all__"
 
 class CategorySerializer(serializers.ModelSerializer):
     posts = PostSerializer(many=True, read_only=True)
     class Meta:
         model = Category
-        fields =['status','posts']
+        fields ="__all__"
         
